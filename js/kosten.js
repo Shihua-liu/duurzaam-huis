2a55594159aa6deda143f286b3d699dbc76cf1bf
shihua liu <liushihua2004@gmail.com>
2021-03-26 16:16:14 +0100

    js van kosten berekenen af met werkende js en buttons

---
+++ js/kosten.js
@@ -0,0 +1,85 @@
+var elekt_laatste;
+var water_laatste;
+var gas_laatste;
+var elek_kost;
+var water_kost;
+var gas_kost;
+const elekt = document.getElementById("elek");
+const water = document.getElementById("water");
+const gas = document.getElementById("gas");
+const button = document.getElementById("submit");
+const button_r = document.getElementById("reset");
+document.getElementById("reset").style.display = "none"; 
+
+button.addEventListener("click", () => {
+    let elekt_stand = parseInt(elekt.value)
+    let elek_totaal = elekt_stand - elekt_laatste
+    console.log(elek_totaal)
+
+    let water_stand = parseInt(water.value)
+    let water_totaal = water_stand - water_laatste
+    console.log(water_totaal)
+
+    let gas_stand = parseInt(gas.value)
+    let gas_totaal = gas_stand - gas_laatste
+    console.log(gas_totaal)
+    document.getElementById("kosten").style.display = "none"; 
+
+    elek_kost = 100 / 22 * elek_totaal;
+            
+    water_kost = 100 / 80 * water_totaal;
+            
+    gas_kost = 100 / 76 * gas_totaal;
+            
+    kost_totaal = elek_kost + water_kost + gas_kost
+    console.log(kost_totaal)
+    let kost_rond = kost_totaal.toFixed(2);
+    document.getElementById("kost").innerHTML = "kosten is momenteel <br>" + kost_rond + " euro"; 
+    document.getElementById("reset").style.display = "block"; 
+})
+
+button_r.addEventListener("click", () => {
+    document.getElementById("kosten").style.display = "block"; 
+    document.getElementById("reset").style.display = "none"; 
+})
+
+// MAAK een nieuw request object aan
+let xmlhttp = new XMLHttpRequest();
+
+// STATUS bijhouden
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+        // this.responseText is het teruggekomen JSON resultaat.
+        let stand = JSON.parse(this.responseText);
+
+        displayResultaat(stand);
+
+    }
+};
+
+// OPEN en verstuur
+xmlhttp.open("GET", "http://localhost/BO3/php/standen2json.php", true);
+xmlhttp.send();
+
+// MAAK het resultaat op basis van de teruggezonden gegevens
+function displayResultaat(stand) {
+    let tekst = '';
+
+    for (let count = 0; count < stand.length; count++) {
+            document.getElementById("standen").innerHTML = tekst;
+            if (stand[count].item == "elektriciteit"){
+                elekt_laatste = stand[count].stand;
+            }
+            if (stand[count].item == "water"){
+                water_laatste = stand[count].stand;
+            }
+            if (stand[count].item == "gas"){
+                gas_laatste = stand[count].stand;
+            }
+
+
+        }
+}
+
+
