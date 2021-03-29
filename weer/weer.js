bc37bb5615fb0c9cf96a33d7d97a71c3fd177bd8
shihua liu <liushihua2004@gmail.com>
2021-03-29 14:21:36 +0200

    temp van buiten laat nu zien van uit een data base

--- weer/weer.js
+++ weer/weer.js
@@ -1,24 +1,24 @@
-var day;
-switch (new Date().getDay()) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-       day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
+var plaats;
+var datum;
+var minimaal;
+var maximaal;
+
+
+
+// OPEN en verstuur
+xmlhttp.open("GET", "http://localhost/BO3/weer/standen2json.php", true);
+xmlhttp.send();
+
+// MAAK het resultaat op basis van de teruggezonden gegevens
+function displayResultaat(stand) {
+    let tekst = '';
+
+    for (let count = 0; count < stand.length; count++) {
+            tekst = tekst +
+                "plaats: " + stand[count].plaats + "<br>" +
+                "datum: " + stand[count].datum + "<br>" +
+                "minimaal: " + stand[count].minimaal + "&deg;C" + "<br>" +
+                "maximaal: " + stand[count].maximaal + "&deg;C" + "<br>"
+            document.getElementById("weer").innerHTML = tekst;
+        }
 }
-document.getElementById("weer").innerHTML = "Today is " + day;
